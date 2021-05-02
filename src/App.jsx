3086ba95a014cb02f79b02769f406bb65c3c908d
zephyrus21 <pandey.piyus@gmail.com>
2021-05-02 17:19:59 +0530

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Counter from './Counter';
-import './App.css';
 
 const App = () => {
   return (
-    <div className='App'>
+    <div style={{ zoom: '200%', textAlign: 'center' }}>
       <Counter />
     </div>
   );
