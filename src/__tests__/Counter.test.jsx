528e8cbbd243a14d43f3143dece29a423b598f76
zephyrus21 <pandey.piyus@gmail.com>
2021-05-02 17:16:37 +0530

    refactoring tests

--- src/__tests__/Counter.test.jsx
+++ src/__tests__/Counter.test.jsx
@@ -2,34 +2,34 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import Counter from '../Counter';
 
+let getByTestId;
+
+beforeEach(() => {
+  const component = render(<Counter />);
+  getByTestId = component.getByTestId;
+});
+
 test('should render header', () => {
-  const { getByTestId } = render(<Counter />);
   expect(getByTestId('header').textContent).toBe('Counter');
 });
 
 test('counter should initially start with 0', () => {
-  const { getByTestId } = render(<Counter />);
   expect(getByTestId('counter').textContent).toBe('0');
 });
 
 test('input should contains initial value of 1', () => {
-  const { getByTestId } = render(<Counter />);
   expect(getByTestId('input').value).toBe('1');
 });
 
 test('add button renders with +', () => {
-  const { getByTestId } = render(<Counter />);
   expect(getByTestId('add-btn').textContent).toBe('+');
 });
 
 test('subtract button renders with -', () => {
-  const { getByTestId } = render(<Counter />);
   expect(getByTestId('sub-btn').textContent).toBe('-');
 });
 
 test('should change values', () => {
-  const { getByTestId } = render(<Counter />);
-
   expect(getByTestId('input').value).toBe('1');
 
   fireEvent.change(getByTestId('input'), {
@@ -41,8 +41,6 @@ test('should change values', () => {
 });
 
 test('should add 1 to counter', () => {
-  const { getByTestId } = render(<Counter />);
-
   expect(getByTestId('counter').textContent).toBe('0');
 
   fireEvent.click(getByTestId('add-btn'));
@@ -51,8 +49,6 @@ test('should add 1 to counter', () => {
 });
 
 test('should subtract 1 from counter', () => {
-  const { getByTestId } = render(<Counter />);
-
   expect(getByTestId('counter').textContent).toBe('0');
 
   fireEvent.click(getByTestId('sub-btn'));
@@ -61,8 +57,6 @@ test('should subtract 1 from counter', () => {
 });
 
 test('changing input value then clicking + works', () => {
-  const { getByTestId } = render(<Counter />);
-
   fireEvent.change(getByTestId('input'), { target: { value: '5' } });
 
   fireEvent.click(getByTestId('add-btn'));
@@ -71,8 +65,6 @@ test('changing input value then clicking + works', () => {
 });
 
 test('changing input value then clicking - works', () => {
-  const { getByTestId } = render(<Counter />);
-
   fireEvent.change(getByTestId('input'), { target: { value: '5' } });
 
   fireEvent.click(getByTestId('sub-btn'));
